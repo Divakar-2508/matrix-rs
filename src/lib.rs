41a9e39b009912e2b321f5890a5c6a554ac40894
Divakar-2508 <divakaraugust@gmail.com>
2024-04-28 13:37:24 +0530

    added basic opertions and impls

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,40 @@
+use std::ops::{Add, Mul, Sub};
 #[allow(dead_code)]
 use std::ops::{Index, IndexMut};
 
+#[derive(Debug, Clone)]
 pub struct Matrix {
-    row: usize,
-    col: usize,
+    dim: Dimension,
     data: Vec<Vec<i32>>,
 }
 
-enum MatrixError {
+#[derive(Debug, Clone, Copy)]
+pub struct Dimension {
+    row: usize,
+    col: usize,
+}
+
+impl Dimension {
+    fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
+    fn get_row(&self) -> usize {
+        self.row
+    }
+
+    fn get_col(&self) -> usize {
+        self.col
+    }
+}
+
+impl PartialEq for Dimension {
+    fn eq(&self, other: &Self) -> bool {
+        self.row == other.row && self.col == other.col
+    }
+}
+
+#[derive(Debug)]
+pub enum MatrixError {
     InvalidColumnLen,
     DifferentDimensions,
 }
@@ -15,36 +42,64 @@ enum MatrixError {
 impl Matrix {
     fn new(row: usize, col: usize) -> Self {
         let data = vec![vec![0; col]; row];
-        Self { row, col, data }
+        let dim = Dimension::new(row, col);
+        Self { dim, data }
+    }
+
+    fn with_data(data: &[&[i32]]) -> Self {
+        let data: Vec<Vec<i32>> = data.into_iter().map(|row| row.to_vec()).collect();
+        let dim = Dimension::new(data.len(), data[0].len());
+        Self { dim, data }
+    }
+
+    fn new_with_dim(dim: Dimension) -> Self {
+        let data = vec![vec![0; dim.col]; dim.row];
+        Self { dim, data }
     }
 
     fn add_row(&mut self, data: &[i32]) -> Result<(), MatrixError> {
-        if data.len() != self.col {
+        if data.len() != self.dim.col {
             return Err(MatrixError::InvalidColumnLen);
         }
         self.data.push(data.to_vec());
         Ok(())
     }
 
-    fn with_data(data: &[&[i32]]) -> Self {
-        let data: Vec<Vec<i32>> = data.into_iter().map(|row| row.to_vec()).collect();
-        let row = data.len();
-        let col = data[0].len();
-        Self { row, col, data }
+    fn is_square_matrix(&self) -> bool {
+        self.dim.row == self.dim.col
     }
 
+    fn sub_matrix(
+        &self,
+        row_start: usize,
+        col_start: usize,
+        row_size: usize,
+        col_size: usize,
+    ) -> Result<Self, MatrixError> {
+        // if row_start > self.row() || col_start > self.col() || r
+        todo!()
+    }
+
+    // fn determinant(&self) -> Option<usize> {
+    //     if !self.is_square_matrix() {
+    //         return None;
+    //     }
+    //
+    //
+    // }
+
     fn row(&self) -> usize {
-        self.row
+        self.dim.row
     }
 
     fn col(&self) -> usize {
-        self.col
+        self.dim.col
     }
 
     fn add_matrix(&mut self, add_matrix: &Self) -> Result<(), MatrixError> {
         let col = add_matrix.col();
         let row = add_matrix.row();
-        if self.col != col || self.row != row {
+        if self.dim != add_matrix.dim {
             return Err(MatrixError::DifferentDimensions);
         }
 
@@ -97,6 +152,46 @@ fn product_matrices(matrix1: &Matrix, matrix2: &Matrix) -> Result<Matrix, Matrix
     Ok(result_matrix)
 }
 
+fn subtract_matrices(matrix1: &Matrix, matrix2: &Matrix) -> Result<Matrix, MatrixError> {
+    if matrix1.dim != matrix2.dim {
+        return Err(MatrixError::DifferentDimensions);
+    }
+
+    let mut result_matrix = Matrix::new_with_dim(matrix1.dim);
+
+    for i in 0..matrix1.row() {
+        for j in 0..matrix1.col() {
+            result_matrix[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+
+    Ok(result_matrix)
+}
+
+impl Add for Matrix {
+    type Output = Result<Self, MatrixError>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        add_matrices(&self, &rhs)
+    }
+}
+
+impl Sub for Matrix {
+    type Output = Result<Self, MatrixError>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        subtract_matrices(&self, &rhs)
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Result<Self, MatrixError>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        product_matrices(&self, &rhs)
+    }
+}
+
 impl Index<usize> for Matrix {
     type Output = Vec<i32>;
 
@@ -109,4 +204,35 @@ impl IndexMut<usize> for Matrix {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output {
         &mut self.data[index]
     }
+}
+
+impl PartialEq for Matrix {
+    fn eq(&self, other: &Self) -> bool {
+        if self.row() != other.row() || self.col() != other.col() {
+            return false;
+        }
+
+        for i in 0..self.row() {
+            for j in 0..self.col() {
+                if self[i][j] != other[i][j] {
+                    return false;
+                }
+            }
+        }
+
+        true
+    }
+}
+
+#[test]
+fn matrix_test() {
+    let mut matrix = Matrix::with_data(&[&[1, 2, 3], &[4, 5, 6], &[7, 8, 9]]);
+    let matrix_new = matrix.clone();
+
+    matrix.add_matrix(&matrix_new).unwrap();
+
+    assert_eq!(
+        matrix,
+        Matrix::with_data(&[&[2, 4, 6], &[8, 10, 12], &[14, 16, 18]])
+    );
 }
